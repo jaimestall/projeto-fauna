85cd8236aad621fa48f495144da18ab4b199a5ba
Jaime Jose Stall Filho <jaimestall94@gmail.com>
2022-07-24 18:08:06 -0300

    Algumas estilizações e integração com API Whatsapp

--- js/main.js
+++ js/main.js
@@ -4,26 +4,28 @@ const imagens = document.querySelectorAll('.img-animal');
 const description = document.querySelectorAll('.description')
 // seleciona todos os links internos da página
 const linksInternos = document.querySelectorAll('a[href^="#"]');
+// estabelece o primeiro elemento de animais-descricao como ativo
+description[0].classList.add('ativo');
 
 // função que exibe/deixa de exibir a descrição de um animal conforme a imagem clicada
 imagens.forEach((img, index) => {
   img.addEventListener('click', () => {
-    description.forEach(i => i.classList.add('display-none'))
-    description[index].classList.remove('display-none')
+    description.forEach(i => i.classList.remove('ativo'))
+    description[index].classList.add('ativo')
   })
 });
 
 // função que atribui/deixa de atribuir determinada classe para um elemento (neste caso os links internos)
-function handleLink(e) {
-  e.preventDefault();
-  linksInternos.forEach(link => {
-    link.classList.remove('ativo');
-  })
-  e.currentTarget.classList.add('ativo');
-}
-linksInternos.forEach(link => {
-  link.addEventListener('click', handleLink)
-});
+// function handleLink(e) {
+//   e.preventDefault();
+//   linksInternos.forEach(link => {
+//     link.classList.remove('ativo');
+//   })
+//   e.currentTarget.classList.add('ativo');
+// }
+// linksInternos.forEach(link => {
+//   link.addEventListener('click', handleLink)
+// });
 
 // função para atribuir/remover determinada classe da tag HTML ao pressionar determinada tecla
 window.addEventListener('keydown', (e) => {
