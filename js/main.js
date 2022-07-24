2af9f18389bdf6ddabbf297a907f32335714c5b7
Jaime Jose Stall Filho <jaimestall94@gmail.com>
2022-07-24 03:48:15 -0300

    Cria função Javascript para trazer a descrição para o animal selecionado

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-function init() {
-  console.log('Vai dar tudo certo!');
-}
+const imagens = document.querySelectorAll('img');
+const description = document.querySelectorAll('.description')
+
+imagens.forEach((img, index) => {
+  img.addEventListener('click', () => {
+    description.forEach(i => i.classList.add('display-none'))
+    description[index].classList.remove('display-none')
+  })
+});
