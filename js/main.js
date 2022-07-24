18814527427c111ad841baf125be4511521cebaf
Jaime Jose Stall Filho <jaimestall94@gmail.com>
2022-07-24 16:09:35 -0300

    atualização javascript e eventos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
-const imagens = document.querySelectorAll('img');
+// seleciona todas as imagens da página
+const imagens = document.querySelectorAll('.img-animal');
+// seleciona todas as descrições dos animais
 const description = document.querySelectorAll('.description')
+// seleciona todos os links internos da página
+const linksInternos = document.querySelectorAll('a[href^="#"]');
 
+// função que exibe/deixa de exibir a descrição de um animal conforme a imagem clicada
 imagens.forEach((img, index) => {
   img.addEventListener('click', () => {
     description.forEach(i => i.classList.add('display-none'))
     description[index].classList.remove('display-none')
   })
+});
+
+// função que atribui/deixa de atribuir determinada classe para um elemento (neste caso os links internos)
+function handleLink(e) {
+  e.preventDefault();
+  linksInternos.forEach(link => {
+    link.classList.remove('ativo');
+  })
+  e.currentTarget.classList.add('ativo');
+}
+linksInternos.forEach(link => {
+  link.addEventListener('click', handleLink)
+});
+
+// função para atribuir/remover determinada classe da tag HTML ao pressionar determinada tecla
+window.addEventListener('keydown', (e) => {
+  if (e.key === "F8") {
+    document.documentElement.classList.toggle('texto-maior')
+  }
 });
