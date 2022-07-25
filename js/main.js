77ab5e4662833c46f932b15c83ef04de6348bdab
Jaime Jose Stall Filho <jaimestall94@gmail.com>
2022-07-25 03:25:43 -0300

    Finalização Javascript puro do site

--- js/main.js
+++ js/main.js
@@ -1,31 +1,74 @@
-// seleciona todas as imagens da página
-const imagens = document.querySelectorAll('.img-animal');
-// seleciona todas as descrições dos animais
-const description = document.querySelectorAll('.description')
-// seleciona todos os links internos da página
-const linksInternos = document.querySelectorAll('a[href^="#"]');
-// estabelece o primeiro elemento de animais-descricao como ativo
-description[0].classList.add('ativo');
+const activeClass = 'ativo'
 
-// função que exibe/deixa de exibir a descrição de um animal conforme a imagem clicada
-imagens.forEach((img, index) => {
-  img.addEventListener('click', () => {
-    description.forEach(i => i.classList.remove('ativo'))
-    description[index].classList.add('ativo')
-  })
-});
+function smoothScroll() {
+  const linksInternos = document.querySelectorAll('a[href^="#"]');
 
-// função que atribui/deixa de atribuir determinada classe para um elemento (neste caso os links internos)
-function handleLink(e) {
-  e.preventDefault();
-  linksInternos.forEach(link => {
-    link.classList.remove('ativo');
-  })
-  e.currentTarget.classList.add('ativo');
+  // função que atribui/deixa de atribuir determinada classe para um elemento (neste caso os links internos)
+  function scrollToSection(e) {
+    e.preventDefault();
+    const href = e.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  };
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection)
+  });
 }
-linksInternos.forEach(link => {
-  link.addEventListener('click', handleLink)
-});
+smoothScroll();
+
+function showTabMenu() {
+  const imagens = document.querySelectorAll('.img-animal');
+  const description = document.querySelectorAll('.description')
+  description[0].classList.add(activeClass);
+
+  // função que exibe/deixa de exibir a descrição de um animal conforme a imagem clicada
+  imagens.forEach((img, index) => {
+    img.addEventListener('click', () => {
+      description.forEach(i => i.classList.remove(activeClass))
+      description[index].classList.add(activeClass)
+    })
+  });
+}
+showTabMenu();
+
+function showFaq() {
+  const faqItem = document.querySelectorAll('.faq-lista dt');
+  faqItem[0].classList.add(activeClass);
+  faqItem[0].nextElementSibling.classList.add(activeClass);
+
+  function ativaResposta() {
+    this.classList.toggle(activeClass)
+    this.nextElementSibling.classList.toggle(activeClass)
+  }
+  faqItem.forEach((item) => {
+    item.addEventListener('click', ativaResposta)
+  });
+}
+showFaq();
+
+function animacaoScroll() {
+  const sections = document.querySelectorAll('.scroll');
+  if (sections.length) {
+    const windowCorte = window.innerHeight * 0.85
+    function chamaSection() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - windowCorte) < 0;
+        if (isSectionVisible) {
+          section.classList.add('ativo');
+        } else {
+          section.classList.remove('ativo');
+        }
+      })
+    }
+    chamaSection();
+    window.addEventListener('scroll', chamaSection)
+  }
+}
+animacaoScroll();
 
 // função para atribuir/remover determinada classe da tag HTML ao pressionar determinada tecla
 window.addEventListener('keydown', (e) => {
