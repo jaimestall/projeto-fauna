879b57472431b2ef21c21e48b3433c47acaee1cb
Jaime Jose Stall Filho <jaimestall94@gmail.com>
2022-07-25 13:18:55 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 const activeClass = 'ativo'
 
 function smoothScroll() {
+  // Seleciona todos os links cujos atributos 'href' começam com o valor "#"
   const linksInternos = document.querySelectorAll('a[href^="#"]');
-
-  // função que atribui/deixa de atribuir determinada classe para um elemento (neste caso os links internos)
-  function scrollToSection(e) {
-    e.preventDefault();
-    const href = e.currentTarget.getAttribute('href');
-    const section = document.querySelector(href);
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
+  if (linksInternos.length) {
+    linksInternos.forEach((link) => {
+      link.addEventListener('click', scrollToSection)
     });
-  };
-  linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection)
-  });
+    function scrollToSection(e) {
+      e.preventDefault();
+      const href = e.currentTarget.getAttribute('href');
+      const section = document.querySelector(href);
+      section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    };
+  }
 }
 smoothScroll();
 
@@ -23,14 +24,15 @@ function showTabMenu() {
   const imagens = document.querySelectorAll('.img-animal');
   const description = document.querySelectorAll('.description')
   description[0].classList.add(activeClass);
-
-  // função que exibe/deixa de exibir a descrição de um animal conforme a imagem clicada
-  imagens.forEach((img, index) => {
-    img.addEventListener('click', () => {
-      description.forEach(i => i.classList.remove(activeClass))
-      description[index].classList.add(activeClass)
-    })
-  });
+  if (imagens.length && description.length) {
+    // função que exibe/deixa de exibir a descrição de um animal conforme a imagem clicada
+    imagens.forEach((img, index) => {
+      img.addEventListener('click', () => {
+        description.forEach(i => i.classList.remove(activeClass))
+        description[index].classList.add(activeClass)
+      })
+    });
+  }
 }
 showTabMenu();
 
@@ -38,21 +40,23 @@ function showFaq() {
   const faqItem = document.querySelectorAll('.faq-lista dt');
   faqItem[0].classList.add(activeClass);
   faqItem[0].nextElementSibling.classList.add(activeClass);
-
-  function ativaResposta() {
-    this.classList.toggle(activeClass)
-    this.nextElementSibling.classList.toggle(activeClass)
+  if (faqItem.length) {
+    faqItem.forEach((item) => {
+      item.addEventListener('click', ativaResposta)
+    });
+    function ativaResposta() {
+      this.classList.toggle(activeClass)
+      this.nextElementSibling.classList.toggle(activeClass)
+    }
   }
-  faqItem.forEach((item) => {
-    item.addEventListener('click', ativaResposta)
-  });
 }
 showFaq();
 
 function animacaoScroll() {
   const sections = document.querySelectorAll('.scroll');
+  const windowCorte = window.innerHeight * 0.85
+  window.addEventListener('scroll', chamaSection)
   if (sections.length) {
-    const windowCorte = window.innerHeight * 0.85
     function chamaSection() {
       sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top;
@@ -65,12 +69,11 @@ function animacaoScroll() {
       })
     }
     chamaSection();
-    window.addEventListener('scroll', chamaSection)
   }
 }
 animacaoScroll();
 
-// função para atribuir/remover determinada classe da tag HTML ao pressionar determinada tecla
+// Função para atribuir/remover determinada classe da tag HTML ao pressionar determinada tecla
 window.addEventListener('keydown', (e) => {
   if (e.key === "F8") {
     document.documentElement.classList.toggle('texto-maior')
